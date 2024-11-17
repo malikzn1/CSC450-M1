6b6412e8b97eb0ff322e78c19373660fe3b9207f
malikzn1 <zara.malik@csuglobal.edu>
2024-11-17 16:22:41 -0700

    Add files via upload

---
+++ CSC450-M1.java
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Fictional person's information
+    string firstName = "John";
+    string lastName = "Doe";
+    string streetAddress = "1234 Elm Street";
+    string city = "Anytown";
+    string zipCode = "12345";
+
+    // Display the information
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
